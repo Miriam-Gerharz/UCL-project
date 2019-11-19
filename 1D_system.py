578f8943ce3f8d23deb6a9d67b780305c1813566
Miriam-Geharz <gerharz@stud.uni-heidelberg.de>
2019-11-19 11:05:44 +0000

    stable 1D version

--- 1D_system.py
+++ 1D_system.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 21 17:25:19 2019
+Created on Thu Nov 14 20:05:27 2019
 
 @author: miriam
 """
@@ -10,8 +9,7 @@ Created on Mon Oct 21 17:25:19 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-import tools 
-
+import tools
 
 #################
 ### CONSTANTS ###
@@ -30,23 +28,15 @@ T = 300 #K
 
 # frequencies
 omega = np.arange(0, 200, 1e-2)*1e3*2*np.pi # freq for spectrum
-#omega_laser = 10 # freq of laser
-omega_j = np.array([174, 157, 78])*1e3*2*np.pi # freq of mechanical modes
-
-#detuning = omega_j[0] - omega_laser
-
+omega_j = np.array([174260, 157e3, 78e3])*2*np.pi # freq of mechanical modes
 detuning = -300e3 * 2*np.pi #Hz
-omega_laser = 3e8/(1064e-9)*2*np.pi + detuning
-#print("laser freq: ", round(omega_laser*1e-9), 'GHz')
 
 # damping
-Gamma = 0.5e-2*np.array([1, 1, 1]) # mechanical damping
-kappa = 2*np.pi*93e3 #Hz
-print('kapp2/2pi', kappa/4/np.pi)
-print(Gamma)
+Gamma = 0.2317e-2*np.array([1, 1, 1])*2 # mechanical damping
+kappa = 2*np.pi*93458 #Hz
 
 # coupling
-g = np.array([-25,-39,57])*1e3 *2*np.pi # Hz ????
+g = np.array([-25412,-39e3,57e3]) *2*np.pi # Hz ????
 
 # phases
 phi = np.array([0,0,np.pi])
@@ -56,140 +46,66 @@ phi = np.array([0,0,np.pi])
 n_opt = 0
 n_mech = k*T/(hbar * omega_j)
 
-omega_minus = -1*omega # freq for spectrum
-
+#############################################################################
 
 #################
 ### FUNCTIONS ###
 #################
 
-def opt_damp_rate(_kappa, _detuning, _g, _omega_j):
-    return -_g**2 * _kappa * (1/(_kappa**2/4 + (_detuning+_omega_j)**2) - 1/(_kappa**2/4 + (_detuning-_omega_j)**2) )  
 
-def photon_number(_n_j, _Gamma_opt, _Gamma_j):
-    return _n_j * _Gamma_j / (abs(_Gamma_opt) + 2*_Gamma_j)
 
- 
-####################
-### PLOT EXAMPLE ###
-####################
+def print_parameter(param):
+    print()
+    print('Parameter:')
+    print('mechanical frequencies/2pi: ', param[0]/2/np.pi)
+    print('detuning/2pi: ', param[1]/2/np.pi)
+    print('Gamma/2pi: ', param[3]/2/np.pi)
+    print('kappa/2pi: ', param[4]/2/np.pi)
+    print('couplings:')
+    print('g_x, g_y, g_z: ', param[2]/2/np.pi, '*2pi')
+
+########################################################################
     
-### omega plus
-
-# calculate spectrum of q
-param = (omega, omega_j, detuning, g, Gamma, kappa, n_opt, n_mech)
-param2 = (omega, omega_j, detuning, g, Gamma*1e-6, kappa, n_opt, n_mech)
-S_x = tools.spectrum_output(0, param)
-S_y = tools.spectrum_output(1, param)
-S_z = tools.spectrum_output(2, param)
-
-S_x2 = tools.spectrum_output(0, param2)
-#print(S_x-S_x2)
+############    
+### MAIN ###
+############
 
 
-# plot
-plt.plot(omega/(2*np.pi)*1e-3, S_x, label = 'x')
-plt.plot(omega/(2*np.pi)*1e-3, S_y, label = 'y')
-plt.plot(omega/(2*np.pi)*1e-3, S_z, label = 'z')
-#plt.plot(omega/(2*np.pi)*1e-3, S_x2, label = 'x2')
-
-plt.axvline(omega_j[0]/(2*np.pi)*1e-3, color='gray')
-plt.axvline(omega_j[1]/(2*np.pi)*1e-3, color='gray')
-plt.axvline(omega_j[2]/(2*np.pi)*1e-3, color='gray')
-
-
-plt.yscale('log')
-#plt.ylim([1e-25,1e3])
-
-plt.xlabel('omega/2pi [kHz]')
-plt.ylabel('spectrum')
-plt.title('first example')
-plt.legend(loc='best')
-
-#plt.show()
-#plt.savefig('pic/omega_pos')
-#plt.close()
-        
-
-# calculate the area under the curve
-delta_omega = omega[1]-omega[0]
-I_x = np.sum(S_x)*delta_omega
-I_y = np.sum(S_y)*delta_omega
-I_z = np.sum(S_z)*delta_omega
-
-print('n_x (area +): ', round(I_x))
-print('n_y (area +): ', round(I_y))
-print('n_z (area +): ', round(I_z))
-
-
-### omega minus
-# calculate spectrum of q
-param = (omega_minus, omega_j, detuning, g, Gamma, kappa, n_opt, n_mech)
-S_x = tools.spectrum_output(0, param)
-S_y = tools.spectrum_output(1, param)
-S_z = tools.spectrum_output(2, param)
-
-
-# plot
-plt.plot(-omega_minus/(2*np.pi)*1e-3, S_x, label = 'x-')
-plt.plot(-omega_minus/(2*np.pi)*1e-3, S_y, label = 'y-')
-plt.plot(-omega_minus/(2*np.pi)*1e-3, S_z, label = 'z-')
-
-#plt.axvline(-omega_j[0]/(2*np.pi)*1e-3, color='gray')
-#plt.axvline(-omega_j[1]/(2*np.pi)*1e-3, color='gray')
-#plt.axvline(-omega_j[2]/(2*np.pi)*1e-3, color='gray')
-
-
-plt.yscale('log')
-#plt.ylim([1e-25,1e3])
-
-plt.xlabel('omega/2pi [kHz]')
-plt.ylabel('spectrum')
-plt.title('first example')
-plt.legend(loc='best')
-
-#plt.show()
-plt.savefig('pic/omega_neg')
-        
-
-# calculate the area under the curve
-delta_omega = omega[1]-omega[0]
-I_x = np.sum(S_x)*delta_omega
-I_y = np.sum(S_y)*delta_omega
-I_z = np.sum(S_z)*delta_omega
-
-print('n_x (area -): ', round(I_x))
-print('n_y (area -): ', round(I_y))
-print('n_z (area -): ', round(I_z))
-
-
-
-
+    
 # optical damping rates
-Gamma_opt_x = opt_damp_rate(kappa, detuning, g[0], omega_j[0])
-Gamma_opt_y = opt_damp_rate(kappa, detuning, g[1], omega_j[1])
-Gamma_opt_z = opt_damp_rate(kappa, detuning, g[2], omega_j[2])
+Gamma_opt = tools.opt_damp_rate(kappa, detuning, g, omega_j)
 
-print()
-print('optical damping rates')
-print('$\Gamma_{opt, x}$:', Gamma_opt_x/1e5, '1e5')
-print('$\Gamma_{opt, y}$:', Gamma_opt_y)
-print('$\Gamma_{opt, z}$:', Gamma_opt_z)
+# photon numbers at equiv
+N = tools.photon_number(n_mech, Gamma_opt, Gamma)
 
-print('mechanical damping')
-print('X', Gamma[0])
 
-# photon numbers
-n_theo_x = photon_number(n_mech[0], Gamma_opt_x, Gamma[0])
-n_theo_y = photon_number(n_mech[1], Gamma_opt_y, Gamma[1])
-n_theo_z = photon_number(n_mech[2], Gamma_opt_z, Gamma[2])
+param = (omega_j, detuning, g, Gamma, kappa, n_opt, n_mech)
+print_parameter(param)
+    
+SXX_plus = tools.spectrum_output(omega, 0, param)
+SXX_minus = tools.spectrum_output(-omega, 0, param)
+SYY_plus = tools.spectrum_output(omega, 1, param)
+SYY_minus = tools.spectrum_output(-omega, 1, param)
+SZZ_plus = tools.spectrum_output(omega, 2, param)
+SZZ_minus = tools.spectrum_output(-omega, 2, param)
 
-print('theoretical photon numbers at equiv')
-print('n_x theo: ', round(n_theo_x, 1))
-print('n_y theo: ', round(n_theo_y, 1))
-print('n_z theo: ', round(n_theo_z, 1))
+plt.plot(omega/2/np.pi*1e-3, SXX_plus, color = 'red', label = 'x')
+plt.plot(-omega/2/np.pi*1e-3, SXX_minus, color = 'orange')
+plt.plot(omega/2/np.pi*1e-3, SYY_plus, color = 'blue', label = 'y')
+plt.plot(-omega/2/np.pi*1e-3, SYY_minus, color = 'cyan')
+plt.plot(omega/2/np.pi*1e-3, SZZ_plus, color = 'green', label = 'z')
+plt.plot(-omega/2/np.pi*1e-3, SZZ_minus, color = 'lawngreen')
 
-print('theoretical photon numbers at room')
-print('n_x theo: ', round(n_mech[0]/1e8, 1), '1e8')
-print('n_y theo: ', round(n_mech[1], 1))
-print('n_z theo: ', round(n_mech[2], 1))
+plt.xlabel('$\omega/(2\pi)$ [Hz]')
+plt.ylabel('$S_{ii}$ [a.u.]')
+plt.legend(loc = 'best')
+plt.savefig('pic/spectrum_1D_calc')
+plt.show()
+
+
+# calculate photon numbers from area
+Delta = np.abs(omega[1])-np.abs(omega[0])
+NX_from_area = tools.n_from_area(SXX_plus, SXX_minus, Delta, N[0], 'X')
+NY_from_area = tools.n_from_area(SYY_plus, SYY_minus, Delta, N[1], 'Y')
+NZ_from_area = tools.n_from_area(SZZ_plus, SZZ_minus, Delta, N[2], 'Z')
+
--- tools.py
+++ tools.py
@@ -1,46 +1,32 @@
 import numpy as np
 
 
+'''
+comments:
+index i: function of omega
+index j: different operators
+
+'''
+
+
 ### operators (only 1D by now)
-def a_in(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[0] = 1 * np.ones(len(_omega))
-    return array
+a_in = np.zeros(8)
+a_in_d = np.zeros(8)
+b1_in = np.zeros(8)
+b1_in_d = np.zeros(8)
+b2_in = np.zeros(8)
+b2_in_d = np.zeros(8)
+b3_in = np.zeros(8)
+b3_in_d = np.zeros(8)
 
-def a_in_d(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[1] = 1 * np.ones(len(_omega))
-    return array
-
-def b1_in(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[2] = 1 * np.ones(len(_omega))
-    return array
-
-def b1_in_d(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[3] = 1 * np.ones(len(_omega))
-    return array
-
-def b2_in(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[4] = 1 * np.ones(len(_omega))
-    return array
-
-def b2_in_d(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[5] = 1 * np.ones(len(_omega))
-    return array
-
-def b3_in(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[6] = 1 * np.ones(len(_omega))
-    return array
-
-def b3_in_d(_omega):
-    array = np.array([[0 for j in range(len(_omega))] for i in range(8)])    
-    array[7] = 1 * np.ones(len(_omega))
-    return array
+a_in[0] = 1
+a_in_d[1] = 1
+b1_in[2] = 1
+b1_in_d[3] = 1
+b2_in[4] = 1
+b2_in_d[5] = 1
+b3_in[6] = 1
+b3_in_d[7] = 1
 
 ### SUSCEPTIBILITIES ###
 # chi
@@ -48,8 +34,8 @@ def chi(_omega, _omega_j, _Gamma):
     return 1/(-1j*(_omega-_omega_j) + _Gamma/2.0)
 
 # optical
-def eta(_omega, detuning, _phi, _kappa):
-    return np.exp(-1j*_phi)*chi(_omega, -detuning, _kappa) - np.exp(1j*_phi)*np.conj(chi(-_omega, -detuning, _kappa))
+def eta(_omega, _detuning, _phi, _kappa):
+    return np.exp(-1j*_phi)*chi(_omega, -_detuning, _kappa) - np.exp(1j*_phi)*np.conj(chi(-_omega, -_detuning, _kappa))
                   
 # mechanical 
 def mu(_omega, _omega_j, _Gamma):
@@ -57,23 +43,19 @@ def mu(_omega, _omega_j, _Gamma):
     mu2 = chi(_omega, _omega_j[1], _Gamma[1]) - np.conj(chi(-_omega, _omega_j[1], _Gamma[1]))
     mu3 = chi(_omega, _omega_j[2], _Gamma[2]) - np.conj(chi(-_omega, _omega_j[2], _Gamma[2]))
     return [mu1, mu2, mu3]
-
+    
 ### NOISES ###
 # optical
 def Q_opt(_omega, _detuning, _kappa, _phi):
-#_omega: array
-#        freq range in which spectrum is to be computed
-#_phi: float
-#        phase
-# 
-    return np.exp(-1j*_phi)*np.einsum('i, ji', chi(_omega, -_detuning, _kappa), a_in(_omega)) + np.exp(1j*_phi)*np.einsum('i, ji', np.conj(chi(-_omega, -_detuning, _kappa)),a_in_d(_omega))
+    return np.exp(-1j*_phi)*np.einsum('i, j->ji', chi(_omega, -_detuning, _kappa), a_in) + np.exp(1j*_phi)*np.einsum('i, j->ji', np.conj(chi(-_omega, -_detuning, _kappa)), a_in_d)
 
 # mechanical
-def Q_mech(_omega, _omega_j, _Gamma, _phi):
-    Q1 = np.einsum('i, ji-> ji', chi(_omega, _omega_j[0], _Gamma[0]),b1_in(_omega)) + np.einsum('i,ji -> ji',np.conj(chi(-_omega, _omega_j[0], _Gamma[0])),b1_in_d(_omega))
-    Q2 = np.einsum('i, ji-> ji', chi(_omega, _omega_j[1], _Gamma[1]),b2_in(_omega)) + np.einsum('i,ji -> ji',np.conj(chi(-_omega, _omega_j[1], _Gamma[1])),b2_in_d(_omega))
-    Q3 = np.einsum('i, ji-> ji', chi(_omega, _omega_j[2], _Gamma[2]),b3_in(_omega)) + np.einsum('i,ji -> ji',np.conj(chi(-_omega, _omega_j[2], _Gamma[2])),b3_in_d(_omega))
+def Q_mech(_omega, _omega_j, _Gamma):
+    Q1 = np.einsum('i, j-> ji', chi(_omega, _omega_j[0], _Gamma[0]),b1_in) + np.einsum('i,j -> ji', np.conj(chi(-_omega, _omega_j[0], _Gamma[0])),b1_in_d)
+    Q2 = np.einsum('i, j-> ji', chi(_omega, _omega_j[1], _Gamma[1]),b2_in) + np.einsum('i,j -> ji', np.conj(chi(-_omega, _omega_j[1], _Gamma[1])),b2_in_d)
+    Q3 = np.einsum('i, j-> ji', chi(_omega, _omega_j[2], _Gamma[2]),b3_in) + np.einsum('i,j -> ji', np.conj(chi(-_omega, _omega_j[2], _Gamma[2])),b3_in_d)
     return [Q1, Q2, Q3]
+    #return [Q1]
 
 ### normalization factor
 def M(_omega, _omega_j, _detuning, _phi, _Gamma, _kappa, _g):
@@ -85,21 +67,20 @@ def M(_omega, _omega_j, _detuning, _phi, _Gamma, _kappa, _g):
 ### displacement operator
 def q(_omega, _omega_j, _detuning, _g, _Gamma, _kappa, _phi):
     _M = M(_omega, _omega_j, _detuning, _phi, _Gamma, _kappa, _g)
-    _Q_mech = Q_mech(_omega, _omega_j, _Gamma, _phi)
+    _Q_mech = Q_mech(_omega, _omega_j, _Gamma)
     _mu = mu(_omega, _omega_j, _Gamma)
     
-    q1 = np.sqrt(_Gamma[0])*np.einsum('i,ji -> ji',1/_M[0], _Q_mech[0]) + 1j*np.sqrt(_kappa)*_g[0]*np.einsum('i, i, j->ji',1/_M[0], _mu[0], Q_opt(_omega, _detuning, _kappa, _phi[0]))
-    q2 = np.sqrt(_Gamma[1])*np.einsum('i,ji -> ji',1/_M[1], _Q_mech[1]) + 1j*np.sqrt(_kappa)*_g[1]*np.einsum('i, i, j->ji',1/_M[1], _mu[1], Q_opt(_omega, _detuning, _kappa, _phi[1]))
-    q3 = np.sqrt(_Gamma[2])*np.einsum('i,ji -> ji',1/_M[2], _Q_mech[2]) + 1j*np.sqrt(_kappa)*_g[2]*np.einsum('i, i, j->ji',1/_M[2], _mu[2], Q_opt(_omega, _detuning, _kappa, _phi[2]))
+    q1 = np.sqrt(_Gamma[0])*np.einsum('i,ji -> ji',1/_M[0], _Q_mech[0]) + 1j*np.sqrt(_kappa)*_g[0]*np.einsum('i, i, ji->ji',1/_M[0], _mu[0], Q_opt(_omega, _detuning, _kappa, _phi[0]))
+    q2 = np.sqrt(_Gamma[1])*np.einsum('i,ji -> ji',1/_M[1], _Q_mech[1]) + 1j*np.sqrt(_kappa)*_g[1]*np.einsum('i, i, ji->ji',1/_M[1], _mu[1], Q_opt(_omega, _detuning, _kappa, _phi[1]))
+    q3 = np.sqrt(_Gamma[2])*np.einsum('i,ji -> ji',1/_M[2], _Q_mech[2]) + 1j*np.sqrt(_kappa)*_g[2]*np.einsum('i, i, ji->ji',1/_M[2], _mu[2], Q_opt(_omega, _detuning, _kappa, _phi[2]))
 
-    
     return [q1, q2, q3]
     
 ### helper
 def expectation_value(_operator, _n, _pair):
-    return _n* np.abs(_operator[_pair])**2 + (_n+1)* np.abs(_operator[_pair+1])**2
+    return (_n+1)* np.abs(_operator[2*_pair])**2 + _n * np.abs(_operator[2*_pair+1])**2
 
-def spectrum_2(_operator, _n_opt, _n_mech):
+def spectrum(_operator, _n_opt, _n_mech):
     s_a = expectation_value(_operator, _n_opt, 0)
     s_b1 = expectation_value(_operator, _n_mech[0], 1)
     s_b2 = expectation_value(_operator, _n_mech[1], 2)
@@ -107,27 +88,83 @@ def spectrum_2(_operator, _n_opt, _n_mech):
     return s_a + s_b1 + s_b2 + s_b3
 
 
-def spectrum_output(_i, param):
+def spectrum_output(omega, _i, param):
     # define phases
     _phi = np.array([0,0,np.pi/2])
     
     # unpack parameters
-    omega = param[0]
-    omega_j = param[1]
-    detuning = param[2]
-    g = param[3]
-    Gamma = param[4]
-    kappa = param[5]
-    n_opt = param[6]
-    n_mech = param[7]
-    
-    #print(param[4])
-    
+    omega_j = param[0]
+    detuning = param[1]
+    g = param[2]
+    Gamma = param[3]
+    kappa = param[4]
+    n_opt = param[5]
+    n_mech = param[6]
     
     # calculate q operator
     operator = q(omega, omega_j, detuning, g, Gamma, kappa, _phi)[_i]
     
-    _spectrum = spectrum_2(operator, n_opt, n_mech)
-    return g[_i]**2 * np.abs(eta(omega, detuning, _phi[_i], kappa))**2 * _spectrum
+    # calculate spectrum
+    _spectrum = spectrum(operator, n_opt, n_mech)
+    
+    return _spectrum
 
 
+
+
+def area(_S, _Delta):     
+
+#  integrate the position spectrum of bead
+# quick hack - use trapezoidal rule- improve later
+    summe=0
+    #Delta=np.abs(omega[1]-omega[0])
+    for i in range(len(_S)-1):
+        Tem = 0.5*(_S[i]+_S[i+1])
+        summe = summe + Tem
+    return summe*_Delta / (2*np.pi) #/2pi for normalization
+   
+    
+
+def n_from_area(_S_plus, _S_minus, _Delta, _N, _name):
+    N_X_plus = area(_S_plus, _Delta)
+    N_X_minus = area(_S_minus, _Delta)
+    N_X = (N_X_plus + N_X_minus -1)/2
+    
+    print()
+    print(_name, 'photon number from area, difference')
+    print('+:', round(N_X_plus, 2), '(', round((N_X_plus-_N)/(_N+1)*100, 2), '% )')
+    print('-:', round(N_X_minus, 2), '(', round((N_X_minus-_N)/(_N)*100, 2), '% )')
+    print('summed:', round(N_X, 2), '(', round((N_X-_N)/(_N)*100, 2), '% )')
+    #print('area -', area(SXX_minus, omega))
+    return [N_X_plus, N_X_minus, N_X]
+
+def opt_damp_rate(_kappa, _detuning, _g, _omega_j):
+    Gamma_opt = -_g**2 * _kappa * (1/(_kappa**2/4 + (_detuning+_omega_j)**2) - 1/(_kappa**2/4 + (_detuning-_omega_j)**2) )  
+
+    print()
+    print('optical damping rates')
+    print('$\Gamma_{opt, x}$:', round(Gamma_opt[0]/1e5, 3), '1e5')
+    print('$\Gamma_{opt, y}$:', round(Gamma_opt[1]/1e5, 3), '1e5')
+    print('$\Gamma_{opt, z}$:', round(Gamma_opt[2]/1e5, 3), '1e5')
+    
+#    print('mechanical damping')
+#    print('X', Gamma[0])
+    
+    return Gamma_opt
+
+
+def photon_number(_n_j, _Gamma_opt, _Gamma_j):
+    N = _n_j * _Gamma_j / (abs(_Gamma_opt) + 2*_Gamma_j)
+    
+    print()
+    print('theoretical photon numbers at equiv')
+    print('n_x theo: ', round(N[0], 3))
+    print('n_y theo: ', round(N[1], 3))
+    print('n_z theo: ', round(N[2], 3))
+    
+    print('theoretical photon numbers at room temperature')
+    print('n_x theo: ', round(_n_j[0]/1e8, 3), '1e8')
+    print('n_y theo: ', round(_n_j[1]/1e8, 3), '1e8')
+    print('n_z theo: ', round(_n_j[2]/1e8, 3), '1e8')
+    
+    return N
